53990c39464801354849c2d6a5b251daea0ee2be
Renan Prata <renancprata@gmail.com>
2019-03-11 19:22:00 -0300

    fixing assert impl

--- src/lib/libc/exit/assert.c
+++ src/lib/libc/exit/assert.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <assert.h>
+#include <error.h>
 
 void __assert_fail(const char * expr, const char * file, int line, const char * func)
 {
 	fprintf(stderr, "Assertion failed: %s (%s: %s: %d)\n", expr, file, func, line);
+	while(1);
 }
